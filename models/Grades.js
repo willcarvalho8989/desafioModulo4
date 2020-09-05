b99d06e19aff07b20e047e7700fffe1871d1af0f
william <williamdcarvalho@gmail.com>
2020-09-05 16:22:28 -0300

    Inicio

---
+++ models/Grades.js
@@ -0,0 +1,28 @@
+import { db } from '../models/index.js';
+
+const gradesSchema = db.mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+  lastModified: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Grades = db.mongoose.model('grades', gradesSchema, 'grades');
+
+export default Grades;
